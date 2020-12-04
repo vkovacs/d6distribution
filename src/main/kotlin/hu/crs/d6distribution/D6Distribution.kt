0bdd180c12bdf345f8314a811055fa06dcf82180
Viktor Kovacs <crusader8@gmail.com>
2020-12-04 20:46:05 +0100

    initial commit

---
+++ src/main/kotlin/hu/crs/d6distribution/D6Distribution.kt
@@ -0,0 +1,23 @@
+@file:Suppress("LocalVariableName")
+
+package hu.crs.d6distribution
+
+fun main() {
+    val distribution = mutableMapOf<Int, Int>()
+
+    for (i in 1..1000000) {
+        val d6_1 = (1..6).random()
+        val d6_2 = (1..6).random()
+        val d6_3 = (1..6).random()
+
+        val sum_d6_1_2 = d6_1 + d6_2
+        val sum_d6_1_3 = d6_1 + d6_3
+        val sum_d6_2_3 = d6_2 + d6_3
+
+        distribution[sum_d6_1_2] = (distribution[sum_d6_1_2] ?: 0) + 1
+        distribution[sum_d6_1_3] = (distribution[sum_d6_1_3] ?: 0) + 1
+        distribution[sum_d6_2_3] = (distribution[sum_d6_2_3] ?: 0) + 1
+    }
+
+    println(distribution.toSortedMap())
+}
